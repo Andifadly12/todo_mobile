870c4b4fbecb8a27229fabdd30c51b597b01d647
Andifadly12 <andifadly2004@gmail.com>
2026-09-06 03:24:30 +0800

    feat: add rounded navigation footer for tasks and profile

---
+++ lib/core/widgets/organisms/app_footer.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../../theme/app_theme.dart';
+
+class AppFooter extends StatelessWidget {
+  const AppFooter({super.key, required this.index, required this.onSelected});
+  final int index;
+  final ValueChanged<int> onSelected;
+  @override
+  Widget build(BuildContext context) => SafeArea(
+    top: false,
+    child: Container(
+      margin: const EdgeInsets.fromLTRB(20, 8, 20, 12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(28),
+        boxShadow: const [
+          BoxShadow(
+            color: Color(0x14825B42),
+            blurRadius: 24,
+            offset: Offset(0, 6),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(28),
+        child: NavigationBar(
+          selectedIndex: index,
+          onDestinationSelected: onSelected,
+          backgroundColor: Colors.white,
+          indicatorColor: AppColors.cream,
+          elevation: 0,
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.checklist_rounded),
+              selectedIcon: Icon(Icons.task_alt),
+              label: 'Tugas',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.person_outline),
+              selectedIcon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
