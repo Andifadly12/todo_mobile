055061562d8a28681e91336a865cea5534d3222b
Andifadly12 <andifadly2004@gmail.com>
2026-09-06 10:39:54 +0800

    feat: integrate category tab into home

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../core/widgets/organisms/app_footer.dart';
+import '../../categories/presentation/category_page.dart';
 import '../../profile/presentation/profile_page.dart';
 import '../../todos/presentation/todo_page.dart';
 
@@ -12,12 +13,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // Preserve the existing post-authentication destination: Profile.
-  int index = 1;
+  int index = 2;
   @override
   Widget build(BuildContext context) => Scaffold(
     body: IndexedStack(
       index: index,
-      children: const [TodoPage(), ProfilePage()],
+      children: const [TodoPage(), CategoryPage(), ProfilePage()],
     ),
     bottomNavigationBar: AppFooter(
       index: index,
